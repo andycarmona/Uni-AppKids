b06599b302570314eee3f424895d9d04c8e97272
andca <andres.carmona@uni-app.se>
2014-11-18 17:40:47 +0100

    View phrases event showing properties when clicking on words

--- Uni-AppKids.Database/Migrations/201411181611338_DescriptionForWords.Designer.cs
+++ Uni-AppKids.Database/Migrations/201411181611338_DescriptionForWords.Designer.cs
@@ -5,13 +5,13 @@ namespace Uni_AppKids.Database.Migrations
     using System.Data.Entity.Migrations.Infrastructure;
     using System.Resources;
     
-    public sealed partial class Added_ArrayInts_with_WordId : IMigrationMetadata
+    public sealed partial class DescriptionForWords : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Added_ArrayInts_with_WordId));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(DescriptionForWords));
         
         string IMigrationMetadata.Id
         {
-            get { return "201409251500191_Added_ArrayInts_with_WordId"; }
+            get { return "201411181611338_DescriptionForWords"; }
         }
         
         string IMigrationMetadata.Source
--- Uni-AppKids.Database/Migrations/201411181611338_DescriptionForWords.cs
+++ Uni-AppKids.Database/Migrations/201411181611338_DescriptionForWords.cs
@@ -3,16 +3,16 @@ namespace Uni_AppKids.Database.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class Added_ArrayInts_with_WordId : DbMigration
+    public partial class DescriptionForWords : DbMigration
     {
         public override void Up()
         {
-            AddColumn("dbo.Phrases", "WordsIds", c => c.String());
+            AddColumn("dbo.Words", "Description", c => c.String());
         }
         
         public override void Down()
         {
-            DropColumn("dbo.Phrases", "WordsIds");
+            DropColumn("dbo.Words", "Description");
         }
     }
 }
